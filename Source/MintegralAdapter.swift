499d1c6062b8a0b7bdbf4178d746f10cbee0100d
Kendall Rogers <kendall.rogers@chartboost.com>
2023-02-07 11:06:24 -0800

    HB-5085: Rename HeliumError

--- Source/MintegralAdapter.swift
+++ Source/MintegralAdapter.swift
@@ -144,7 +144,7 @@ final class MintegralAdapter: PartnerAdapter {
     /// A default implementation is provided that returns `nil`.
     /// Only implement if the partner SDK provides its own list of error codes that can be mapped to Helium's.
     /// If some case cannot be mapped return `nil` to let Helium choose a default error code.
-    func mapLoadError(_ error: Error) -> HeliumError.Code? {
+    func mapLoadError(_ error: Error) -> ChartboostMediationError.Code? {
         guard (error as NSError).domain == kMTGErrorDomain,
               let code = MTGErrorCode(rawValue: (error as NSError).code) else {
             return nil
@@ -197,7 +197,7 @@ final class MintegralAdapter: PartnerAdapter {
     /// A default implementation is provided that returns `nil`.
     /// Only implement if the partner SDK provides its own list of error codes that can be mapped to Helium's.
     /// If some case cannot be mapped return `nil` to let Helium choose a default error code.
-    func mapShowError(_ error: Error) -> HeliumError.Code? {
+    func mapShowError(_ error: Error) -> ChartboostMediationError.Code? {
         guard (error as NSError).domain == kMTGErrorDomain,
               let code = MTGErrorCode(rawValue: (error as NSError).code) else {
             return nil
