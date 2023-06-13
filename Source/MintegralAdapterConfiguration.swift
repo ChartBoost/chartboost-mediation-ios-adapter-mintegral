cc66afe008cbdcd15342197993ab39b1d4f376c4
Daniel Barros <daniel.barros@chartboost.com>
2023-06-13 10:20:11 -0700

    Update file headers

--- Source/MintegralAdapterConfiguration.swift
+++ Source/MintegralAdapterConfiguration.swift
@@ -1,12 +1,7 @@
 // Copyright 2022-2023 Chartboost, Inc.
-// 
+//
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
-
-//
-//  MintegralAdapterConfiguration.swift
-//  ChartboostMediationAdapterMintegral
-//
 
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
 @objc public class MintegralAdapterConfiguration: NSObject {
