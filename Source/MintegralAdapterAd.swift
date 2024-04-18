e8b5529b9d838e6fa2ce79e7c0a4df2d355f0bde
Daniel Barros <daniel.barros@chartboost.com>
2024-04-18 11:22:46 +0530

    Fix linting issues

--- Source/MintegralAdapterAd.swift
+++ Source/MintegralAdapterAd.swift
@@ -18,11 +18,13 @@ class MintegralAdapterAd: NSObject {
     var details: PartnerDetails = [:]
 
     /// The ad load request associated to the ad.
-    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)`` or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
+    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)``
+    /// or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
     let request: PartnerAdLoadRequest
         
     /// The partner ad delegate to send ad life-cycle events to.
-    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)`` or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
+    /// It should be the one provided on ``PartnerAdapter/makeBannerAd(request:delegate:)``
+    /// or ``PartnerAdapter/makeFullscreenAd(request:delegate:)``.
     weak var delegate: PartnerAdDelegate?
     
     /// Mintegral's Unit ID needed to load an ad.
