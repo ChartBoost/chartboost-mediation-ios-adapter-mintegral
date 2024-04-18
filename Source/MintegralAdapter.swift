ea922568a4f5bbb2616bf519407eacc3b37bd2ca
Helium Service <103450738+helium-service@users.noreply.github.com>
2024-04-18 08:54:16 -0700

    Update Version to 4.7.6.0.0 (#43)
    * [AUTO-GENERATED] Update version to 4.7.6.0.0
    
    * [AUTO-GENERATED] Update copyright headers
    
    * fix spacing
    
    ---------
    
    Co-authored-by: runner <runner@Mac-1712047709405.local>
    Co-authored-by: nadia.elmouldi <nadia.elmouldi@chartboost.com>

--- Source/MintegralAdapter.swift
+++ Source/MintegralAdapter.swift
@@ -18,8 +18,8 @@ final class MintegralAdapter: PartnerAdapter {
     /// The version of the adapter.
     /// It should have either 5 or 6 digits separated by periods, where the first digit is Chartboost Mediation SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
     /// Format: `<Chartboost Mediation major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
-    let adapterVersion = "4.7.5.0.0"
-    
+    let adapterVersion = "4.7.6.0.0"
+
     /// The partner's unique identifier.
     let partnerIdentifier = "mintegral"
     
@@ -204,10 +204,7 @@ final class MintegralAdapter: PartnerAdapter {
                 .kMTGErrorCodeImageURLisEmpty,
                 .kMTGErrorCodeNoSupportPopupWindow,
                 .kMTGErrorCodeFailedDiskIO,
-                .kMTGErrorCodeSocketIO,
-                .kMTGErrorCodeAdsCountInvalid,
-                .kMTGErrorCodeSocketInvalidStatus,
-                .kMTGErrorCodeSocketInvalidContent:
+                .kMTGErrorCodeSocketIO:
             return .loadFailureUnknown
         @unknown default:
             return nil
@@ -260,10 +257,7 @@ final class MintegralAdapter: PartnerAdapter {
                 .kMTGErrorCodeImageURLisEmpty,
                 .kMTGErrorCodeNoSupportPopupWindow,
                 .kMTGErrorCodeFailedDiskIO,
-                .kMTGErrorCodeSocketIO,
-                .kMTGErrorCodeAdsCountInvalid,
-                .kMTGErrorCodeSocketInvalidStatus,
-                .kMTGErrorCodeSocketInvalidContent:
+                .kMTGErrorCodeSocketIO:
             return .showFailureUnknown
         @unknown default:
             return nil
