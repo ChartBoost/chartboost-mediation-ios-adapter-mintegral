a37b99eabbeb86fcb5bdc663f169d85f1e8aa36b
Daniel Barros <daniel.barros@chartboost.com>
2024-04-22 10:35:46 -0700

    Remove unnecessary return keywords

--- Source/MintegralAdapter.swift
+++ Source/MintegralAdapter.swift
@@ -125,7 +125,7 @@ final class MintegralAdapter: PartnerAdapter {
     func makeBannerAd(request: PartnerAdLoadRequest, delegate: PartnerAdDelegate) throws -> PartnerBannerAd {
         // Multiple banner loads are allowed so a banner prefetch can happen during auto-refresh.
         // ChartboostMediationSDK 5.x does not support loading more than 2 banners with the same placement, and the partner may or may not support it.
-        return try MintegralAdapterBannerAd(adapter: self, request: request, delegate: delegate)
+        try MintegralAdapterBannerAd(adapter: self, request: request, delegate: delegate)
     }
 
     /// Creates a new ad object in charge of communicating with a single partner SDK ad instance.
